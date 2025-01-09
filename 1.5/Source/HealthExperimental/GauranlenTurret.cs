aa8189ff918a56375c69ceb44f69030a938db997
RedMattis <redmattis@gmail.com>
2025-01-09 18:57:00 +0100

    Some performance improvements for the turrets since they could get a bit expensive if you had tons. Mostly improved performance by preventing them from being as "acquire-target" eager as vanilla turrets.
    Buffed carrier dryads slightly.

--- 1.5/Source/HealthExperimental/GauranlenTurret.cs
+++ 1.5/Source/HealthExperimental/GauranlenTurret.cs
@@ -11,6 +11,10 @@ namespace Dryad
     public class GauranlenTurret : Building_TurretGun
     {
         public bool targetingFire = false;
+        private const int Building_TurretGun_TryStartShootSomethingIntervalTicks = 10;
+        public const int TryTargetFireTick = Building_TurretGun_TryStartShootSomethingIntervalTicks * 30;
+
+
         protected override void BeginBurst()
         {
             if (targetingFire)
@@ -25,7 +29,10 @@ namespace Dryad
         }
 
         // Get first verb able to taget fires.
-        protected Verb ExtinguishVerb => GunCompEq.AllVerbs.FirstOrDefault(v => v.verbProps.targetParams.canTargetFires);
+        private Verb _extinguishVerb = null;
+
+        // Without a verb this will constantly check. Don't add this to a turret without this type of verb, the whole point of this class is to target fires.
+        protected Verb ExtinguishVerb => _extinguishVerb ??= GunCompEq.AllVerbs.FirstOrDefault(v => v.verbProps.targetParams.canTargetFires);
 
         /// <summary>
         /// For the fire extinguish-sap ability
@@ -35,15 +42,46 @@ namespace Dryad
         {
             var target = base.TryFindNewTarget();
             targetingFire = false;
-            if (target.IsValid && ExtinguishVerb != null)
+            if (target.IsValid)
             {
                 return target;
             }
+            if (ExtinguishVerb == null) return LocalTargetInfo.Invalid;
             targetingFire = true;
 
             var verb = ExtinguishVerb;
 
+            
+            
+            TryGetPositionsWithFire(verb);
+            if (positionsWithFire.Count > 0)
+            {
+                var targetPos = positionsWithFire.First();
+                // Refreshing every 10 frames while there is a fire should be fine. TPS is probably not a concern when stuff is literally on fire.
+                TryGetPositionsWithFire(verb, force: true);
+
+                return targetPos;
+            }
+
+            return LocalTargetInfo.Invalid;
+        }
+
+        protected List<IntVec3> positionsWithFire = [];
+        public int timeSinceLastCheck = 0;
+        public const int FireCheckInterval = 10;
+        private void TryGetPositionsWithFire(Verb verb, bool force=false)
+        {
+            if (force) timeSinceLastCheck = 99999;
+            
+            // Building_TurretGun_TryStartShootSomethingIntervalTicks
+            if (timeSinceLastCheck < FireCheckInterval)
+            {
+                timeSinceLastCheck++;
+                return;
+            }
+            timeSinceLastCheck = 0;
             int num = GenRadial.NumCellsInRadius(verb.verbProps.range);
+            positionsWithFire.Clear();
             for (int i = 0; i < num; i++)
             {
                 IntVec3 intVec = base.Position + GenRadial.RadialPattern[i];
@@ -56,11 +94,10 @@ namespace Dryad
                 {
                     if (thingList[j] is Fire || thingList[j].HasAttachment(ThingDefOf.Fire))
                     {
-                        return thingList[j].Position;
+                        positionsWithFire.Add(thingList[j].Position);
                     }
                 }
             }
-            return LocalTargetInfo.Invalid;
         }
 
         public override void ExposeData()
